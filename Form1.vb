f1b9f1006cc44169f8d2f3d4f869657e5376c670
苦力怕控 <2956226491@qq.com>
2019-06-08 22:26:23 +0800

    fix gui

--- Form1.vb
+++ Form1.vb
@@ -6,4 +6,12 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         MessageBox.Show("后悔了是吧？没门！", "faq", MessageBoxButtons.OK, MessageBoxIcon.Error)
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub bgm()
+        My.Computer.Audio.Play(My.Resources.Resource1.bgm, AudioPlayMode.BackgroundLoop)
+    End Sub
 End Class
